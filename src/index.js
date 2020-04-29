d8ff550e4f9b96e0f62821d47b44fb6734021f81
faizalabidin <64177276+faizalabidin@users.noreply.github.com>
2020-04-29 07:54:07 +0700

    3 files

--- src/index.js
+++ src/index.js
@@ -5,10 +5,55 @@ import Gallery from './Gallery';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Gallery />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <React.StrictMode>
+  //   <Gallery />
+  // </React.StrictMode>
+  
+  <div class="container">
+    
+    <div class="block"> </div>
+    <div class="block1"> </div>
+    <div class="block2"> </div>
+    <div class="block3"> </div>
+    <div class="block4"> </div>
+    <div class="block5"> </div>
+    <div class="block6"> </div>
+    <div class="block7"> </div>
+    <div class="block8"> </div>
+    <div class="block9"> </div>
+    <div class="block10"> </div>
+    <div class="block11"> </div>
+    <div class="block12"> </div>
+    <div class="block13"> </div>
+    <div class="block14"> </div>
+    <div class="block15"> </div>
+    <div class="block16"> </div>
+    <div class="block17"> </div>
+    <div class="block18"> </div>
+    <div class="block19"> </div>
+    <div class="block20"> </div>
+    <div class="block21"> </div>
+    <div class="block22"> </div>
+    <div class="block23"> </div>
+    <div class="block24"> </div>
+    <div class="block25"> </div>
+    <div class="block26"> </div>
+    <div class="block27"> </div>
+    <div class="block28"> </div>
+    <div class="block29"> </div>
+    <div class="block30"> </div>
+    <div class="block31"> </div>
+    <div class="block32"> </div>
+    <div class="block33"> </div>
+    <div class="block34"> </div>
+    <div class="block35"> </div>
+    <div class="block36"> </div>
+    <div class="block37"> </div>
+    <div class="block38"> </div>
+    <div class="block39"> </div>
+
+  </div>
+  ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
